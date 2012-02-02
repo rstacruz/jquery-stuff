ea08032d7178d4dd574c842c007f95c5f5d8c69a
Rico Sta. Cruz <rico@ambiescent.com>
2012-02-02 23:20:40 +0800

    Ensurevisible: allow offsets.

--- ensurevisible/jquery.ensurevisible.coffee
+++ ensurevisible/jquery.ensurevisible.coffee
@@ -1,27 +1,36 @@
 # ### jQuery.fn.ensureVisible([speed], [callback])
+# ### jQuery.fn.ensureVisible({ offset: 30, speed: 50 }, [speed], [callback])
 # Ensures that a given item is visible in its scroll pane.
-jQuery.fn.ensureVisible = (speed, cb) ->
-  [speed, cb] = [null, speed]  if typeof speed == 'function'
-  speed ?= 400
+jQuery.fn.ensureVisible = ->
+  args = Array::slice.apply arguments
+  opts =
+    speed: 400
+    offset: 0
+
+  $.extend opts,  args.shift()  if typeof args[0] is 'object'
+  opts.speed    = args.shift()  if typeof args[0] is 'number'
+  opts.callback = args.shift()  if typeof args[0] is 'function'
 
   parent = @offsetParent()
   oflow  = parent.css('overflow-y') or parent.css('overflow')
 
-  return this  unless oflow == 'auto' or oflow == 'scroll'
+  return this  unless oflow is 'auto' or oflow is 'scroll'
 
   paneMin    = parent.scrollTop()
   paneMax    = paneMin + parent.innerHeight()
 
-  itemMin    = @.position().top + paneMin
-  itemMax    = itemMin + @outerHeight()
+  itemMin    = @position().top + paneMin - opts.offset
+  itemMax    = itemMin + @outerHeight() + opts.offset
 
   if itemMax > paneMax
-    parent.stop().animate scrollTop: itemMax - parent.innerHeight(), speed, cb
+    parent.stop().animate scrollTop: itemMax - parent.innerHeight(),
+      opts.speed, opts.callback
 
   else if itemMin < paneMin
-    parent.stop().animate scrollTop: itemMin, speed, cb
+    parent.stop().animate scrollTop: itemMin,
+      opts.speed, opts.callback
 
   else
-    cb()  if typeof cb == 'function'
+    opts.callback()  if typeof opts.callback is 'function'
 
   this
--- ensurevisible/jquery.ensurevisible.js
+++ ensurevisible/jquery.ensurevisible.js
@@ -1,36 +1,34 @@
 (function() {
-  jQuery.fn.ensureVisible = function(speed, cb) {
-    var itemMax, itemMin, oflow, paneMax, paneMin, parent, _ref;
-    if (typeof speed === 'function') {
-      _ref = [null, speed], speed = _ref[0], cb = _ref[1];
-    }
-        if (speed != null) {
-      speed;
-    } else {
-      speed = 400;
+
+  jQuery.fn.ensureVisible = function() {
+    var args, itemMax, itemMin, oflow, opts, paneMax, paneMin, parent;
+    args = Array.prototype.slice.apply(arguments);
+    opts = {
+      speed: 400,
+      offset: 0
     };
+    if (typeof args[0] === 'object') $.extend(opts, args.shift());
+    if (typeof args[0] === 'number') opts.speed = args.shift();
+    if (typeof args[0] === 'function') opts.callback = args.shift();
     parent = this.offsetParent();
     oflow = parent.css('overflow-y') || parent.css('overflow');
-    if (!(oflow === 'auto' || oflow === 'scroll')) {
-      return this;
-    }
+    if (!(oflow === 'auto' || oflow === 'scroll')) return this;
     paneMin = parent.scrollTop();
     paneMax = paneMin + parent.innerHeight();
-    itemMin = this.position().top + paneMin;
-    itemMax = itemMin + this.outerHeight();
+    itemMin = this.position().top + paneMin - opts.offset;
+    itemMax = itemMin + this.outerHeight() + opts.offset;
     if (itemMax > paneMax) {
       parent.stop().animate({
         scrollTop: itemMax - parent.innerHeight()
-      }, speed, cb);
+      }, opts.speed, opts.callback);
     } else if (itemMin < paneMin) {
       parent.stop().animate({
         scrollTop: itemMin
-      }, speed, cb);
+      }, opts.speed, opts.callback);
     } else {
-      if (typeof cb === 'function') {
-        cb();
-      }
+      if (typeof opts.callback === 'function') opts.callback();
     }
     return this;
   };
+
 }).call(this);
