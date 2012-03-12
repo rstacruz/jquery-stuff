76d9713dacc0daa2d8edbd684867aa6bfe848dde
Rico Sta. Cruz <rico@ambiescent.com>
2012-03-12 16:05:17 +0800

    Autogrow: trailing spaces cleanup.

--- autogrow/jquery.autogrow.js
+++ autogrow/jquery.autogrow.js
@@ -9,13 +9,13 @@
     $.fn.autogrow = function(options) {
         if (!options) { options = {}; }
         if (!options.offset) { options.offset = 0; }
-        
+
         this.filter('textarea').each(function() {
-            
+
             var $this       = $(this),
                 minHeight   = $this.height(),
                 lineHeight  = $this.css('lineHeight');
-            
+
             var shadow = $('<div></div>').css({
                 position:   'absolute',
                 top:        -10000,
@@ -26,9 +26,9 @@
                 lineHeight: $this.css('lineHeight'),
                 resize:     'none'
             }).appendTo(document.body);
-            
+
             var update = function() {
-    
+
                 var times = function(string, number) {
                     var _res = '';
                     for(var i = 0; i < number; i ++) {
@@ -36,27 +36,27 @@
                     }
                     return _res;
                 };
-                
+
                 var val = this.value.replace(/</g, '&lt;')
                                     .replace(/>/g, '&gt;')
                                     .replace(/&/g, '&amp;')
                                     .replace(/\n$/, '<br/>&nbsp;')
                                     .replace(/\n/g, '<br/>')
                                     .replace(/ {2,}/g, function(space) { return times('&nbsp;', space.length -1) + ' ' });
-                
+
                 shadow.html(val);
                 $(this).css('height', Math.max(shadow.height() + 20 + options.offset, minHeight));
-            
+
             }
-            
+
             $(this).change(update).keyup(update).keydown(update);
-            
+
             update.apply(this);
-            
+
         });
-        
+
         return this;
-        
+
     }
-    
+
 })(jQuery);
