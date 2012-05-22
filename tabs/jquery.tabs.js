1bc0fae76e136bf51bf6cec34e6ebe42e4a20dac
Rico Sta. Cruz <rico@ambiescent.com>
2012-05-22 17:13:04 +0800

    Update jQuery tabs to support .tabbed({ activate: true }).

--- tabs/jquery.tabs.js
+++ tabs/jquery.tabs.js
@@ -1,11 +1,18 @@
+/*! ---------------------------------------
+ *  Tabs
+ *  http://github.com/rstacruz/jquery-stuff
+ * ---------------------------------------- */
+
 // Simple tabs.
 //
 //   // All options are optional.
 //   $(".tabbed").tabbed();
 //
 //   $(".tabbed").tabbed({
-//     content: '.content',
-//     links: '[href]'
+//     content: '.content',  /* Selector for content areas. */
+//     links: '[href]',      /* Selector for tab links. */
+//     active: 'active',     /* Classname for active tabs/content areas. */
+//     activate: false       /* Activate the tabs on start. Default: false */
 //   });
 //
 // http://github.com/rstacruz/jquery-stuff
@@ -15,33 +22,43 @@
     var defaults = {
       content: '> *:not(:first-child)',
       links: '[href]',
-      active: 'active'
+      active: 'active',
+      activate: false
     };
 
     options || (options = {});
     options = $.extend({}, defaults, options);
 
-    var self = this;
+    var self = $(this);
 
-    function update(target) {
-      self.find(options.links)
+    // Selects the tab called `target` in the parent element `$parent`.
+    function select($parent, target) {
+      $parent.find(options.links)
         .removeClass(options.active)
         .filter("[href='#"+target+"']")
           .addClass(options.active);
 
-      self.find(options.content).hide();
-      self.find('#'+target).show();
+      $parent.find(options.content).hide();
+      $parent.find('#'+target).show();
     }
 
-    this.on("click", "[href]", function(e) {
+    // Register the click handler to delegate to the select() function.
+    self.on("click", "[href]", function(e) {
       e.preventDefault();
       var target = $(this).attr('href').substr(1);
-      update(target);
+      var $parent = $(this).closest(self);
+      select($parent, target);
     });
 
-    var $active = self.find(options.links).filter('.'+options.active);
-    if ($active.length) {
-      update($active.attr('href').substr(1));
+    // Activate the tabs if it's asked for.
+    if (options.activate) {
+      self.each(function() {
+        var $parent = this;
+        var $active = $parent.find(options.links).filter('.'+options.active);
+        if ($active.length) {
+          update($active.attr('href').substr(1));
+        }
+      });
     }
   };
 
