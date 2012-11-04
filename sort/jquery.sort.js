005544ad56c08dff60fe66af0e89c506b68b1b89
Rico Sta. Cruz <rico@ambiescent.com>
2012-11-05 04:49:44 +0800

    Remove coffee version of sort.

--- sort/jquery.sort.js
+++ sort/jquery.sort.js
@@ -1,26 +1,34 @@
-(function() {
-  jQuery.fn.sort = function(options) {
-    var list, order, parent, sel, via;
-    if (options == null) {
-      options = {};
-    }
-    via = options.via || function(el) {
-      return el.text();
-    };
-    sel = options.items || '*';
-    sel = "> " + sel;
-    order = options.order || 'asc';
-    parent = $(this);
-    list = [];
-    parent.find(sel).each(function() {
-      return list.push($(this).remove());
-    });
+/*! jQuery.sort (c) 2012, Rico Sta. Cruz. MIT License.
+ *  https://github.com/rstacruz/jquery-stuff/tree/master/sort */
+
+// $.fn.sort()
+// Sorts the insides of a given element.
+// Requires underscore.js.
+//
+// Options:
+//
+//  * `via` (function, required) - should return the value to be sorted.
+//  * `items` (string) - the selector of the children. defaults to '*'.
+//  * `order` (string) - the order ('desc' or 'asc'). defaults to 'asc'.
+
+(function($, _) {
+  $.fn.sort = function(options) {
+    if (!options) options = {};
+
+    var via    = options.via || function(el) { return el.text(); };
+    var sel    = "> " + (options.items || '*');
+    var order  = options.order || 'asc';
+    var parent = $(this);
+    var list   = [];
+
+    // Build the list of elements while taking them out of the DOM.
+    parent.find(sel).each(function() { list.push($(this).remove()); });
+
+    // Sort that list then add them back in.
     list = _.sortBy(list, via);
-    if (order === 'desc') {
-      list.reverse();
-    }
-    return _.each(list, function(element) {
-      return parent.append(element);
-    });
+    if (order === 'desc') list.reverse();
+    _.each(list, function(element) { parent.append(element); });
+
+    return this;
   };
-}).call(this);
+})(jQuery, _);
