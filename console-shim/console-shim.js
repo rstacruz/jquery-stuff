99a75d386a9b74a98f6993f680e0c8745b92191f
Rico Sta. Cruz <rico@ambiescent.com>
2012-11-12 15:51:22 +0800

    Update comment in console shim.

--- console-shim/console-shim.js
+++ console-shim/console-shim.js
@@ -1,8 +1,8 @@
 /*! Console Shim (c) 2012, Rico Sta. Cruz. MIT License.
  *   http://github.com/rstacruz/jquery-stuff/tree/master/console-shim */
 
-// Makes `console.log()` commands silently fail if the current browser doesn't
-// have console API support.
+// Makes `console.log()` commands silently not fail if the current browser
+// doesn't have console API support.
 
 (function (console) {
   var methods = 'assert count debug dir dirxml error group groupCollapsed groupEnd info log markTimeline profile profileEnd table time timeEnd trace warn'.split('');
