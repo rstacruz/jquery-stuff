56eeba51bba21f7dd075b93d8a7910858b3e0191
Rico Sta. Cruz <rico@ambiescent.com>
2012-12-08 15:30:30 +0800

    Fadeonload: refactor to not need extra CSS.

--- fadeonload/jquery.fadeonload.js
+++ fadeonload/jquery.fadeonload.js
@@ -1,39 +1,36 @@
+/*! fadeonload (c) 2012, Rico Sta. Cruz. MIT License.
+ *  http://github.com/rstacruz/jquery-stuff/tree/master/fadeonload */
+
 // Fades an image in on load.
 //
 //     $('.wallpaper img').fadeonload();
 //
 // The image doesn't have to be in the DOM at time of calling.
 //
-// It would be best if the image has `opacity: 0` in the CSS as well so there
-// won't be a "flash of visible image".
-//
 (function($) {
+  function addStyles(styles) {
+    $('head').append($("<style type='text/css'>").html(styles));
+  }
 
   $.fn.fadeonload = function() {
     var $image = this;
 
-    $(function() {
-      $image.each(function() {
-        var $image = $(this);
+    // Ensure that the image begins with 0 opacity by adding a new stylesheet.
+    // Note that this will not work for things like `$(".foo").children()`
+    addStyles(this.selector + " { opacity: 0; }");
 
-        if (this.complete) {
-          $image.trigger('load');
-        } else {
-          $image.css({ opacity: 0 });
-        }
-      });
+    // On load, fade it in. Relies on jQ 1.8+ to do vendor-prefix-free
+    // transitions.
+    this.on('load', function() {
+      $(this).css({ opacity: 1, transition: 'opacity 400ms linear' });
     });
 
-    this.on('load', function() {
-      var $image = $(this);
-
-      $image
-        .show()
-        .css({ opacity: 0 });
-
-      setTimeout(function() {
-        $image.css({ opacity: 1, transition: 'opacity 400ms linear' });
-      }, 25);
+    // On document load (or immediately), if images are cached (and hence
+    // "loaded" before the document DOM), manually trigger their fading in.
+    $(function() {
+      $image.each(function() {
+        if (this.complete) $(this).trigger('load');
+      });
     });
 
     return this;
