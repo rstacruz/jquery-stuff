5136a8642935984cde9f6241fec0b15e518b3913
Rico Sta. Cruz <rico@ambiescent.com>
2012-12-09 04:43:44 +0800

    Autoexpand: rewrite to accomodate binding before DOM elements exist.

--- autoexpand/jquery.autoexpand.js
+++ autoexpand/jquery.autoexpand.js
@@ -9,24 +9,38 @@
 //
 // Also, you can define the minimum height via CSS's `min-height`.
 //
-// Preferred usage
+// Suggested usage
 // ---------------
 //
-// For those that are multiline by default, this (below) works well. The extra
-// padding makes it inviting to add more text.
+// This is a sensible default:
 //
-//     $("#myform textarea").autoexpand({ extraLines: 1 });
+//     $("textarea.autoexpand").autoexpand({ extraLines: 1 });
+//     $("textarea.autoexpand-single").autoexpand();
 //
-// For multiline textboxes that masquerade as non-multilines, just use it as
-// is.
+// Make your multiline textareas `.autoexpand`. The extra padding makes it
+// inviting to type more text in.
 //
-//     $("#myform textarea").autoexpand();
+// For single-line textareas, add `.autoexpand-single` to them. This is for
+// textareas that are just one-line high but are supposed to expand to more
+// lines, like the Facebook comments area.
 //
-// First based on: http://code.google.com/p/gaequery/source/browse/trunk/src/static/scripts/jquery.autogrow-textarea.js?r=2
+// If you're going to create these textareas dynamically, or update their
+// content dynamically, you'll need to retrigger the autoexpand event to resize
+// it as needed.
+//
+//     $("textarea").trigger('autoexpand');
+//
+// Acknowledgements
+// ----------------
+//
+// First based on:
+// http://code.google.com/p/gaequery/source/browse/trunk/src/static/scripts/jquery.autogrow-textarea.js?r=2
 
 (function($) {
 
   $.fn.autoexpand = function(options) {
+    var $this = this;
+
     var defaults = {
       extraLines: 0,     /* Extra padding (in number of lines) */
       scroll: false,     /* Hide scrollbars if false (default). Set to `true` to leave scrollbars alone. */
@@ -41,21 +55,53 @@
     // weird, no matter what.
     if (options.extraLines === 0) options.speed = 0;
 
-    this.filter('textarea').each(function() {
+    // The event handler that updates the 'shadow' div -- done on every
+    // window resize to handle resizing of the textarea.
+    var updateShadow = function() {
       var $textarea = $(this);
 
-      // Sanity check: don't apply the events more than once. If .autoexpand() is
-      // called again, it will merely update the height as needed.
-      if ($textarea.data('autoexpand')) {
-        $textarea.trigger('autoexpand');
-        return;
+      // Sanity check: don't do anything if called prematurely
+      if (!$textarea.length) return;
+
+      var $shadow = $textarea.data('shadow');
+
+      // Create the shadow element if not available
+      if (!$shadow) {
+        $shadow = $('<div class="autoexpand-shadow">').appendTo(document.body);
+        initTextarea($textarea);
+        $textarea.data('shadow', $shadow);
       }
 
-      $textarea.data('autoexpand', true);
+      // This determines how we set the width/height of the elements
+      var isFull = ($textarea.css('box-sizing') === 'border-box');
 
-      // Create a 'shadow' div that has the same style as the original.
-      var $shadow = $('<div class="autoexpand-shadow">').appendTo(document.body);
+      var minHeight = int($textarea.css('min-height'));
+      if (options.useHeight) {
+        var height = isFull ? $textarea.outerHeight() : $textarea.css('height');
+        minHeight = Math.max(minHeight, int(height));
+      }
 
+      $shadow.css({
+        position:  'absolute',
+        top:        -10000,
+        left:       -10000,
+        zIndex:     -100,
+        width:      isFull ? $textarea.outerWidth() : $textarea.css('width'),
+        minHeight:  minHeight,
+        font:       $textarea.css('font'),
+        boxSizing:  $textarea.css('box-sizing'),
+        border:     $textarea.css('border'),
+        padding:    $textarea.css('padding'),
+        wordWrap:   $textarea.css('word-wrap'),
+        whiteSpace: $textarea.css('white-space'),
+        visibility: 'hidden',
+        resize:     'none'
+      });
+
+      return $shadow;
+    };
+
+    var initTextarea = function($textarea) {
       // Disable the resize gripper, manually resizing will interfere with
       // the autoexpand logic
       $textarea.css({ 'resize': 'none' });
@@ -66,87 +112,72 @@
       if (options.speed) {
         addTransition($textarea, 'height ' + options.speed + 'ms ease-in');
       }
+    };
 
-      var minHeight, maxHeight, isFull;
+    var updateHeight = function() {
+      var $textarea = $(this);
 
-      // The event handler that updates the 'shadow' div -- done on every
-      // window resize to handle resizing of the textarea.
-      var updateShadow = function() {
-        // This determines how we set the width/height of the elements.
-        isFull = ($textarea.css('box-sizing') === 'border-box');
+      // Sanity check: don't do anything if called prematurely
+      if (!$textarea.length) return;
 
-        maxHeight = parseInt($textarea.css('max-height'), 10);
-        if (isNaN(maxHeight)) maxHeight = null;
+      // Get or build the shadow
+      var $shadow = $textarea.data('shadow') || updateShadow.apply(this);
 
-        minHeight = parseInt($textarea.css('min-height'), 10);
-        if (options.useHeight) {
-          var height = isFull ? $textarea.outerHeight() : $textarea.css('height');
-          minHeight = Math.max(minHeight, parseInt(height, 10));
+      // Build the value for the shadow
+      var val = htmlescape($textarea.val());
+      if (options.extraLines > 0) val += times('<br/>&nbsp;', options.extraLines);
+
+      $shadow.html(val);
+
+      // This determines how we set the width/height of the elements
+      var isFull = ($textarea.css('box-sizing') === 'border-box');
+
+      // Find the target height
+      var height = isFull ? $shadow.outerHeight() : $shadow.css('height');
+      var maxHeight = int($textarea.css('max-height'));
+      if ((maxHeight) && (height >= maxHeight)) height = maxHeight;
+
+      $textarea.css('height', height);
+
+      // If we've reached your max-height, show the scrollbars
+      if ((maxHeight !== null) && (scroll === false)) {
+        if (height === maxHeight) {
+          $textarea.css({ 'overflow-y': 'auto' });
+        } else {
+          $textarea.css({ 'overflow-y': 'hidden' });
         }
-
-        $shadow.css({
-          position:  'absolute',
-          top:        -10000,
-          left:       -10000,
-          zIndex:     -100,
-          width:      isFull ? $textarea.outerWidth() : $textarea.css('width'),
-          minHeight:  minHeight,
-          font:       $textarea.css('font'),
-          boxSizing:  $textarea.css('box-sizing'),
-          border:     $textarea.css('border'),
-          padding:    $textarea.css('padding'),
-          wordWrap:   $textarea.css('word-wrap'),
-          whiteSpace: $textarea.css('white-space'),
-          visibility: 'hidden',
-          resize:     'none'
-        });
-      };
-
-      // The event handler for updates: update the 'shadow' box with the same
-      // text and use its height for the textarea height.
-      var updateHeight = function() {
-        var val = htmlescape($textarea.val());
-        if (options.extraLines > 0) val += times('<br/>&nbsp;', options.extraLines);
-
-        $shadow.html(val);
-
-        var height = isFull ? $shadow.outerHeight() : $shadow.css('height');
-        if ((maxHeight) && (height >= maxHeight)) height = maxHeight;
-
-        $textarea.css('height', height);
-
-        // If you've reached your max-height, show the scrollbars.
-        if ((maxHeight !== null) && (scroll === false)) {
-          if (height === maxHeight) {
-            $textarea.css({ 'overflow-y': 'auto' });
-          } else {
-            $textarea.css({ 'overflow-y': 'hidden' });
-          }
-        }
-      };
-
-      // Underscore.js bonus! If underscore.js is available, don't call the updating logic
-      // too often to improve performance for fast typers.
-      if (typeof window._ === 'function' && typeof _.throttle === 'function') {
-        updateHeight = _.throttle(updateHeight, 25);
-        updateShadow = _.throttle(updateShadow, 25);
       }
+    };
 
-      var updateAll = function() { updateShadow(); updateHeight(); };
+    // Underscore.js bonus! If underscore.js is available, don't call the updating logic
+    // too often to improve performance for fast typers.
+    if (typeof window._ === 'function' && typeof _.throttle === 'function') {
+      updateHeight = _.throttle(updateHeight, 25);
+      updateShadow = _.throttle(updateShadow, 25);
+    }
 
-      // Run the update immediately
-      updateAll();
+    var updateAll = function() { updateShadow.apply(this); updateHeight.apply(this); };
 
-      // Bind things
-      $textarea.on('change keyup', updateHeight);
-      $(window).on('resize', updateAll);
+    // Bind events. Need to use `.live` instead of `.on` because if there are
+    // matches during its existence, it will not match future elements that may
+    // appear later.
+    this.live('change keyup focus', updateHeight);
+    $(window).on('resize', function() { $this.each(updateAll); });
 
-      // Allow manually updating the height via `.trigger('autoexpand')`
-      $textarea.on('autoexpand', updateAll);
-    });
+    // Allow manually updating the height via `.trigger('autoexpand')`
+    this.live('autoexpand', updateAll);
+
+    // Trigger immediately
+    $(function() { $this.each(updateAll); });
 
     return this;
   };
+
+  // Converts a string (like "32px") to integer.
+  function int(str) {
+    var i = parseInt(str, 10);
+    return isNaN(i) ? null : i;
+  }
 
   // Escapes a `string` to HTML entities.
   function htmlescape(string) {
